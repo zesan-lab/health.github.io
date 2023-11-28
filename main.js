dc2046f88566f4c080c5b79c42778ea03ecc99bb
zesan-lab <152298908+zesan-lab@users.noreply.github.com>
2023-11-28 14:54:42 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+// Get the navigation element
+var navBar = document.getElementById('main-nav');
+
+// Add event listeners for mouseover and mouseout
+navBar.addEventListener('mouseover', function () {
+    // Change the background color on mouseover
+    navBar.style.backgroundColor = '#05794c'; // Change this to match your hover color
+});
+
+navBar.addEventListener('mouseout', function () {
+    // Change the background color back on mouseout
+    navBar.style.backgroundColor = '#05794c'; // Change this to your default color
+});
+
+
+  
